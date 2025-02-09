2505db4b051a5b2ec615d5185e6ab6b8e74d8baf
Alejandro Barrero <krote95@gmail.com>
2025-02-09 19:02:29 +0100

    Ejercicios DOM JavaScript

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -9,16 +9,55 @@ boton.innerHTML = "Obtener Informacion Click";
 
 document.body.appendChild(boton);
 
+boton.addEventListener("click", function(event) {
+    console.log(event);
+});
 
 
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
+let eventoFocus = document.querySelector(".focus");
 
-
+eventoFocus.addEventListener("focus", function() {
+    console.log(eventoFocus.value);
+});
 
 
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
 
+let eventoInput = document.querySelector(".value");
+
+eventoInput.addEventListener("input", function() {
+    console.log(eventoInput.value);
+});
 
 
+/* Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
+que imprima cada uno de los albums.
+const albums = [
+    "De Mysteriis Dom Sathanas",
+    "Reign of Blood",
+    "Ride the Lightning",
+    "Painkiller",
+    "Iron Fist",
+]; */
 
+const albums = [
+    "De Mysteriis Dom Sathanas",
+    "Reign of Blood",
+    "Ride the Lightning",
+    "Painkiller",
+    "Iron Fist",
+];
+
+let listaUl = document.createElement("ul");
+
+albums.forEach(elemento => {
+    let listaLi = document.createElement("li");
+
+    listaLi.innerHTML = elemento;
+
+    listaUl.appendChild(listaLi);
+});
+
+document.body.appendChild(listaUl);
